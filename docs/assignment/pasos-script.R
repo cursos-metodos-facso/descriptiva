c8b3bef92fd44271fbb765a36315ae778a65afde
mafenunez <146782034+mafenunez@users.noreply.github.com>
2026-04-19 13:46:18 -0400

    practico 4

---
+++ docs/assignment/pasos-script.R
@@ -0,0 +1,26 @@
+# ******************************************************************************
+#
+#                               TALLER DE R
+#                         Esdística Descriptiva 2026
+#
+#                             Fecha:
+#                            Nombre:
+#
+#
+# ******************************************************************************
+
+# 0. Cargar librerías
+
+# 1. Importar datos
+
+# 2. Procesamiento de datos
+
+## 2.1 Seleccionar
+
+## 2.2 Explorar
+
+## 2.3 Recodificar
+
+## 2.4 Tratamiento de NA
+
+# 3. Guardar base procesada
